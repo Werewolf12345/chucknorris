0725b22e5f55e8837b4b27503419fd377d0ce727
Alexey Boriskin <alexei.boriskin@gmail.com>
2017-08-16 16:20:49 -0500

    Initial commit

---
+++ src/main/java/com/alexboriskin/chucknorris/ChucknorrisApplication.java
@@ -0,0 +1,12 @@
+package com.alexboriskin.chucknorris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChucknorrisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChucknorrisApplication.class, args);
+	}
+}
